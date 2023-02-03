5333b26f7ca22d489dc8962c4eb66074311ad635
Nata Konstanta <rio201150@gmail.com>
2023-02-03 21:17:17 +0500

    Задача 66

--- Program.cs
+++ Program.cs
@@ -31,3 +31,29 @@
 //     }
 // }
 // printAllNaturalNumbers(n);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите начальное число M:");
+// int numberM = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите начальное число M:");
+// int numberN = int.Parse(Console.ReadLine());
+// ///Метод нахождения суммы натуральных элементов в промежутке от M до N
+// void GapNumberSum (int numberM, int numberN, int sum)
+// {
+//     if (numberM > numberN) 
+//     {
+//         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+//         return;
+//     }
+//     sum = sum + (numberM++);
+//     GapNumberSum(numberM, numberN, sum);
+// }
+// GapNumberSum(numberM, numberN, 0);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
